0a4294c901828472f63bcb1455b90043f17dbe53
Nitrous <thenitrous74@gmail.com>
2023-02-26 11:07:08 -0500

    Inserted Scripts

---
+++ Database Scripts/Restaurant_Create_Tables.sql
@@ -0,0 +1,74 @@
+﻿CREATE TABLE Customer
+(
+  cust_id      int  NOT NULL ,
+  cust_first_name    char(50) ,
+  cust_mid_initial char(50)  ,
+  cust_last_name   char(50)  ,
+  cust_email   char(50)  NOT NULL ,
+  is_member   boolean
+);
+
+
+CREATE TABLE Menu
+(
+  food_id  int          NOT NULL ,
+  food_name char(50)          NOT NULL ,
+  beverage_id   int     NOT NULL ,
+  beverage_name   char(50)          NOT NULL
+);
+
+
+CREATE TABLE Food_Order
+(
+  order_id  int          NOT NULL ,
+  cust_name char(50)          NOT NULL ,
+  order_amount   int     NOT NULL ,
+  order_time  int         NOT NULL
+);
+
+
+CREATE TABLE Worker
+(
+  worker_id    int      NOT NULL ,
+  worker_name    char(50)      NOT NULL ,
+  is_working  boolean     NOT NULL ,
+  worker_job_category char(50)  NOT NULL ,
+  worker_salary  int  , 
+  is_full_time  boolean
+);
+
+
+CREATE TABLE Stock_Shipment
+(
+  supplier_id   int NOT NULL ,
+  supplier_name    char(50) NOT NULL ,
+  is_food_stocked boolean  ,
+  is_beverage_stocked  boolean
+);
+
+
+CREATE TABLE Delivery_Driver
+(
+  driver_id   int NOT NULL ,
+  driver_name    char(50) NOT NULL ,
+  is_working boolean  ,
+  driver_vin  int ,
+  delivery_time  int
+);
+
+
+----------------------
+-- Define primary keys
+----------------------
+ALTER TABLE Customer ADD PRIMARY KEY (cust_id);
+ALTER TABLE Food_Order ADD PRIMARY KEY (order_id);
+ALTER TABLE Menu ADD PRIMARY KEY (food_id, beverage_id);
+ALTER TABLE Worker ADD PRIMARY KEY (worker_id);
+ALTER TABLE Stock_Shipment ADD PRIMARY KEY (supplier_id);
+ALTER TABLE Delivery_Driver ADD PRIMARY KEY (driver_id, driver_vin);
+
+
+----------------------
+-- Define foreign keys
+----------------------
+-- ALTER TABLE OrderItems ADD CONSTRAINT FK_OrderItems_Orders FOREIGN KEY (order_num) REFERENCES Orders (order_num);
---
+++ Database Scripts/Restaurant_Populate_Tables.sql
@@ -0,0 +1,56 @@
+﻿---------------------------------------------------
+-- Sams Teach Yourself SQL in 10 Minutes
+-- http://www.forta.com/books/0672325675/
+-- Example table population scripts for PostgreSQL.
+---------------------------------------------------
+
+
+---------------------------
+-- Populate Customer table
+---------------------------
+INSERT INTO Customer(cust_id, cust_first_name, cust_mid_initial, cust_last_name, cust_email, is_member)
+VALUES(01, 'John', 'P', 'Smith', 'john.smith@gmail.com', false);
+INSERT INTO Customer(cust_id, cust_first_name, cust_mid_initial, cust_last_name, cust_email, is_member)
+VALUES(02, 'Phil', 'K', 'Columbus', 'phil2443_amazing@hotmail.com', true);
+
+-------------------------
+-- Populate Menu table
+-------------------------
+INSERT INTO Menu(food_id, food_name, beverage_id, beverage_name)
+VALUES(01,'Pork',01 ,'Fanta');
+INSERT INTO Menu(food_id, food_name, beverage_id, beverage_name)
+VALUES(02,'Chicken',02 ,'Sprite');
+
+--------------------------
+-- Populate Food_Order table
+--------------------------
+INSERT INTO Food_Order(order_id, cust_name, order_amount, order_time)
+VALUES(01, 'John P Smith', 9, 15);
+INSERT INTO Food_Order(order_id, cust_name, order_amount, order_time)
+VALUES(02, 'Phil K Columbus', 2, 25);
+
+------------------------
+-- Populate Worker table
+------------------------
+INSERT INTO Worker(worker_id, worker_name, is_working, worker_job_category, worker_salary, is_full_time)
+VALUES(01, 'Timmy B Susan', true, 'Cashier', 30000, true);
+INSERT INTO Worker(worker_id, worker_name, is_working, worker_job_category, worker_salary, is_full_time)
+VALUES(02, 'Jimmy I Cool', false, 'Manager', 70000, true);
+
+----------------------------
+-- Populate Stock_Shipment table
+----------------------------
+INSERT INTO Stock_Shipment(supplier_id, supplier_name, is_food_stocked, is_beverage_stocked)
+VALUES(01, 'Chris Suppling Company', false, true);
+INSERT INTO Stock_Shipment(supplier_id, supplier_name, is_food_stocked, is_beverage_stocked)
+VALUES(02, 'Amazing Food Co.', true, false);
+
+----------------------------
+-- Populate Delivery_Driver table
+----------------------------
+INSERT INTO Delivery_Driver(driver_id, driver_name, is_working, driver_vin, delivery_time)
+VALUES(01, 'Richcy D Rich', true, 37654276, 25);
+INSERT INTO Delivery_Driver(driver_id, driver_name, is_working, driver_vin, delivery_time)
+VALUES(02, 'Mike P Anderson', true, 45353254, 30);
+
+
